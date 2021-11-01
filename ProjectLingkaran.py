71e9603c0a07f7680e850b92e6c21d00e590bfb9
khrisnaina06 <muhammadkhrisna12@gmail.com>
2021-11-01 19:58:56 +0700

    menambahkan file

---
+++ ProjectLingkaran.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Luas dan Keliling Lingkaran")
+r = int(input("Masukan Jari - Jari = "))
+
+phi = 3.14159
+luas = phi*r*r
+kel = 2*phi*r
+
+print("Luas Lingkaran Adalah \t= ",format(luas,'.2f'))
+print("Keliling Lingkaran Adalah \t= ",format(kel,'.2f'))
